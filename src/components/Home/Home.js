5c9fa0567c525802f85788ae8ac20e613228999e
Kaung Myat Hun <kaungmyathun7@gmail.com>
2023-01-30 00:10:17 +0630

    fixing: error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { BsFillGearFill, BsArrowLeft, BsSearch, BsEmojiLaughing } from "react-icons/bs";
-import InputLabel from "@mui/material/InputLabel";
 import Box from "@mui/material/Box";
-import PersonIcon from "@mui/icons-material/Person";
 import MenuItem from "@mui/material/MenuItem";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { FaUser, FaPen, FaBell, FaPhoneAlt } from "react-icons/fa";
@@ -16,18 +13,10 @@ import axios from "axios";
 import ScrollToBottom from "react-scroll-to-bottom";
 import { useNavigate } from "react-router-dom";
 import InfoIcon from "@mui/icons-material/Info";
-import EmojiPicker from "emoji-picker-react";
 
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -90,6 +79,9 @@ const Home = () => {
             Channel Name
           </div>
           <p style={{ textAlign: "center", fontWeight: 700 }}>{title.name}</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M505.2 324.8l-47.73-68.78l47.75-68.81c7.359-10.62 8.797-24.12 3.844-36.06c-4.969-11.94-15.52-20.44-28.22-22.72l-82.39-14.88l-14.89-82.41c-2.281-12.72-10.76-23.25-22.69-28.22c-11.97-4.936-25.42-3.498-36.12 3.844L256 54.49L187.2 6.709C176.5-.6016 163.1-2.039 151.1 2.896c-11.92 4.971-20.4 15.5-22.7 28.19l-14.89 82.44L31.15 128.4C18.42 130.7 7.854 139.2 2.9 151.2C-2.051 163.1-.5996 176.6 6.775 187.2l47.73 68.78l-47.75 68.81c-7.359 10.62-8.795 24.12-3.844 36.06c4.969 11.94 15.52 20.44 28.22 22.72l82.39 14.88l14.89 82.41c2.297 12.72 10.78 23.25 22.7 28.22c11.95 4.906 25.44 3.531 36.09-3.844L256 457.5l68.83 47.78C331.3 509.7 338.8 512 346.3 512c4.906 0 9.859-.9687 14.56-2.906c11.92-4.969 20.4-15.5 22.7-28.19l14.89-82.44l82.37-14.88c12.73-2.281 23.3-10.78 28.25-22.75C514.1 348.9 512.6 335.4 505.2 324.8zM456.8 339.2l-99.61 18l-18 99.63L256 399.1L172.8 456.8l-18-99.63l-99.61-18L112.9 255.1L55.23 172.8l99.61-18l18-99.63L256 112.9l83.15-57.75l18.02 99.66l99.61 18L399.1 255.1L456.8 339.2zM256 143.1c-61.85 0-111.1 50.14-111.1 111.1c0 61.85 50.15 111.1 111.1 111.1s111.1-50.14 111.1-111.1C367.1 194.1 317.8 143.1 256 143.1zM256 319.1c-35.28 0-63.99-28.71-63.99-63.99S220.7 192 256 192s63.99 28.71 63.99 63.1S291.3 319.1 256 319.1z" />
+          </svg>
           <div
             style={{
               margin: "20px",
@@ -158,13 +150,13 @@ const Home = () => {
   useEffect(() => {
     socket.on("receive_message", (data) => {
       const dataSocket = data.data;
-      console.log("send user name", name);
-      console.log("socket received user name", data.data.user.userName);
+      // console.log("send user name", name);
+      // console.log("socket received user name", data.data.user.userName);
+      console.log("socket data", data.data);
       // console.log(data.data);
       setOldMessages((oldmessages) => [...oldmessages, data?.data]);
-      // console.log("socket ", data.data);
       if (data.data.user.userName !== name) {
-        toast(`${dataSocket.user.userName} : ${dataSocket.message}`);
+        toast(` ${dataSocket.user.userName} : ${dataSocket.message}`);
         audioPlayer.current.play();
       }
       // setNoti(() => data.data.user.userName);
@@ -358,12 +350,16 @@ const Home = () => {
                           {/* {item?.latest_messages[0]?.user?.userName} */}{" "}
                           {item.latest_messages[0]?.user.userName}
                         </span>
-                        <span>
-                          {item?.latest_messages[0].message.length >= 19
-                            ? item?.latest_messages[0]?.message.slice(0, 20) + "....."
-                            : item.latest_messages[0].message}
-                          {/* {item.latest_message[0].message} */}
-                        </span>
+                        {item.latest_messages.length !== 0 ? (
+                          <span>
+                            {item?.latest_messages[0].message.length >= 19
+                              ? item?.latest_messages[0]?.message.slice(0, 20) + "....."
+                              : item.latest_messages[0].message}
+                            {/* {item.latest_message[0].message} */}
+                          </span>
+                        ) : (
+                          <span></span>
+                        )}
                         <span
                           style={{ fontSize: "10px", marginLeft: "90px" }}
                           className={classes.date}>
@@ -386,7 +382,7 @@ const Home = () => {
           {/* Header */}
           <div className={classes.header}>
             <div className={classes.channel1}>
-              <BsArrowLeft style={{ marginRight: "20px" }}></BsArrowLeft>
+              {/* <BsArrowLeft style={{ marginRight: "20px" }}></BsArrowLeft> */}
               {/* <img src={img} style={{ width: "30px", height: "30px" }}></img> */}
               {title?.name !== "" ? <p>{title?.name}</p> : <p></p>}
             </div>

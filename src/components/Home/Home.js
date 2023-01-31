04fe234813e387df5db48502e01333e0e7cabab4
Kaung Myat Hun <kaungmyathun7@gmail.com>
2023-01-31 11:53:21 +0630

    adding: reply

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -43,7 +43,8 @@ const Home = () => {
   const [notiMessage, setNotiMessage] = useState("");
   const [change, setChange] = useState(true);
   const [users, setUsers] = useState([]);
-
+  const [replyMessage, setReplyMessage] = useState(false);
+  const [reply, setReply] = useState({});
   const audioPlayer = useRef(null);
 
   // console.log(name);
@@ -79,9 +80,9 @@ const Home = () => {
             Channel Name
           </div>
           <p style={{ textAlign: "center", fontWeight: 700 }}>{title.name}</p>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M505.2 324.8l-47.73-68.78l47.75-68.81c7.359-10.62 8.797-24.12 3.844-36.06c-4.969-11.94-15.52-20.44-28.22-22.72l-82.39-14.88l-14.89-82.41c-2.281-12.72-10.76-23.25-22.69-28.22c-11.97-4.936-25.42-3.498-36.12 3.844L256 54.49L187.2 6.709C176.5-.6016 163.1-2.039 151.1 2.896c-11.92 4.971-20.4 15.5-22.7 28.19l-14.89 82.44L31.15 128.4C18.42 130.7 7.854 139.2 2.9 151.2C-2.051 163.1-.5996 176.6 6.775 187.2l47.73 68.78l-47.75 68.81c-7.359 10.62-8.795 24.12-3.844 36.06c4.969 11.94 15.52 20.44 28.22 22.72l82.39 14.88l14.89 82.41c2.297 12.72 10.78 23.25 22.7 28.22c11.95 4.906 25.44 3.531 36.09-3.844L256 457.5l68.83 47.78C331.3 509.7 338.8 512 346.3 512c4.906 0 9.859-.9687 14.56-2.906c11.92-4.969 20.4-15.5 22.7-28.19l14.89-82.44l82.37-14.88c12.73-2.281 23.3-10.78 28.25-22.75C514.1 348.9 512.6 335.4 505.2 324.8zM456.8 339.2l-99.61 18l-18 99.63L256 399.1L172.8 456.8l-18-99.63l-99.61-18L112.9 255.1L55.23 172.8l99.61-18l18-99.63L256 112.9l83.15-57.75l18.02 99.66l99.61 18L399.1 255.1L456.8 339.2zM256 143.1c-61.85 0-111.1 50.14-111.1 111.1c0 61.85 50.15 111.1 111.1 111.1s111.1-50.14 111.1-111.1C367.1 194.1 317.8 143.1 256 143.1zM256 319.1c-35.28 0-63.99-28.71-63.99-63.99S220.7 192 256 192s63.99 28.71 63.99 63.1S291.3 319.1 256 319.1z" />
-          </svg>
+          </svg> */}
           <div
             style={{
               margin: "20px",
@@ -165,12 +166,12 @@ const Home = () => {
     });
   }, [socket]);
 
-  useEffect(() => {
-    socket.on("getNotification", (data) => {
-      console.log("TZ");
-      console.log(data);
-    });
-  }, [socket]);
+  // useEffect(() => {
+  //   socket.on("getNotification", (data) => {
+  //     console.log("TZ");
+  //     console.log(data);
+  //   });
+  // }, [socket]);
 
   // useEffect(() => {
   //   oldmessages.reverse();
@@ -342,7 +343,7 @@ const Home = () => {
                     }}>
                     <div className={classes.text}>
                       <div>
-                        <img src={img} style={{ width: "50px" }} />
+                        <img src={img} style={{ width: "50px" }} alt="" />
                       </div>
                       <div className={classes.team}>
                         <p style={{ fontSize: "14px", fontWeight: "bold" }}>{item?.name}</p>
@@ -358,7 +359,7 @@ const Home = () => {
                             {/* {item.latest_message[0].message} */}
                           </span>
                         ) : (
-                          <span></span>
+                          <span>No Messages</span>
                         )}
                         <span
                           style={{ fontSize: "10px", marginLeft: "90px" }}
@@ -439,16 +440,96 @@ const Home = () => {
                       <div className={classes.cir}>{oldmessage?.user?.userName?.slice(0, 1)}</div>
                       <div>
                         {oldmessage?.media ? (
-                          <img src={oldmessage?.media} className={classes.image}></img>
+                          <img src={oldmessage?.media} className={classes.image} alt=""></img>
                         ) : (
                           <>
                             {" "}
-                            <p className={classes.chat}>{`${oldmessage?.message}`}</p>
-                            <span className={classes.time}>{oldmessage?.time}</span>
+                            <div
+                              // style={{ display: "flex", alignItems: "center" }}
+                              className={
+                                oldmessage?.user?.userName === name
+                                  ? `${classes.meReply}`
+                                  : `${classes.otherReply}`
+                              }>
+                              <div>
+                                <p className={classes.chat}>{`${oldmessage?.message}`}</p>
+                                <span className={classes.time}>{oldmessage?.time}</span>
+                              </div>
+                              <div
+                                className={classes.svgReply}
+                                onClick={(e) => {
+                                  setReplyMessage(true);
+                                  setReply({});
+                                }}>
+                                <svg
+                                  width="30px"
+                                  fill="#707070"
+                                  viewBox="0 0 24.00 24.00"
+                                  id="turn-around-left-top-direction-circle-2"
+                                  // data-name="Flat Line"
+                                  class="icon flat-line"
+                                  transform="rotate(180)matrix(-1, 0, 0, 1, 0, 0)"
+                                  stroke="#707070">
+                                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                  <g
+                                    id="SVGRepo_tracerCarrier"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke="#CCCCCC"
+                                    stroke-width="0.288"></g>
+                                  <g id="SVGRepo_iconCarrier">
+                                    <circle
+                                      id="secondary"
+                                      cx="12"
+                                      cy="12"
+                                      r="9"
+                                      style={{
+                                        fill: "#e0fbff",
+                                        strokeWidth: "0.8399999999999999",
+                                      }}></circle>
+                                    <polyline
+                                      id="primary"
+                                      points="10 8 8 10 10 12"
+                                      style={{
+                                        fill: "none",
+                                        stroke: "#6e6e6e",
+                                        strokeLinecap: "round",
+                                        strokeLinejoin: "round",
+                                        strokeWidth: "0.8399999999999999",
+                                      }}></polyline>
+                                    <path
+                                      id="primary-2"
+                                      data-name="primary"
+                                      d="M8,10h5a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h0"
+                                      style={{
+                                        fill: "none",
+                                        stroke: "#6e6e6e",
+                                        strokeLinecap: "round",
+                                        strokeLinejoin: "round",
+                                        strokeWidth: "0.8399999999999999",
+                                      }}></path>
+                                    <circle
+                                      id="primary-3"
+                                      data-name="primary"
+                                      cx="12"
+                                      cy="12"
+                                      r="9"
+                                      style={{
+                                        fill: "none",
+                                        stroke: "#6e6e6e",
+                                        strokeLinecap: "round",
+                                        strokeLinejoin: "round",
+                                        strokeWidth: "0.8399999999999999",
+                                      }}></circle>
+                                  </g>
+                                </svg>
+                              </div>
+                            </div>
                           </>
                         )}
                       </div>
                     </div>
+                    <div></div>
                   </div>
                 );
               })}
@@ -458,10 +539,20 @@ const Home = () => {
           {/* message section end */}
 
           {/* Bottom */}
+          {replyMessage ? (
+            <div className={classes.replyMessageContainer}>
+              <span>Reply Message</span>
+              <span className={classes.replyClos} onClick={() => setReplyMessage(false)}>
+                X
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
           <form className={classes.bottom} onSubmit={sendHandler}>
             <input
               className={classes.input}
-              placeholder="Type a Message"
+              placeholder={replyMessage ? "Reply Message" : "Type a Message"}
               value={messageData}
               onChange={(e) => {
                 setMessageData(e.target.value);
